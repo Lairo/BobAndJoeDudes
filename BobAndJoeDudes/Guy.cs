dd187831b9b57ea6752d993477aa4db9578b11d0
chajus1 <chajus1@googlemail.com>
2024-09-08 04:17:06 +0100

    Filled in

--- BobAndJoeDudes/Guy.cs
+++ BobAndJoeDudes/Guy.cs
@@ -19,12 +19,31 @@ namespace BobAndJoeDudes
 
         public int GiveCash(int amount)
         {
-            return 0;
+            if (amount <= 0)
+            {
+                Console.WriteLine($"{Name} says: {amount} isn't valid amount");
+                return 0;
+            }
+            if (amount > Cash)
+            {
+                Console.WriteLine($"{Name} says: I don't have enough cash to give you {amount}");
+                return 0;
+            }
+            Cash -= amount;
+            return amount;
         }
 
-        public int Receive(int amount)
+        public void ReceiveCash(int amount)
         {
-            return 0;
+            if (amount <= 0)
+            {
+                Console.WriteLine($"{Name} says: {amount} isn't an amount I'll take");
+            }
+            else
+            {
+                Cash += amount;
+            }
+            
         }
     }
 }
--- BobAndJoeDudes/Program.cs
+++ BobAndJoeDudes/Program.cs
@@ -4,10 +4,42 @@
     {
         static void Main()
         {
-            Guy Joe = new Guy { Name = "Joe", Cash = 100 };
-            Guy Bob = new Guy{ Name = "Bob", Cash = 50 };
+            Guy joe = new Guy { Name = "Joe", Cash = 100 };
+            Guy bob = new Guy { Name = "Bob", Cash = 50 };
 
+            while (true)
+            {
+                joe.WriteMyInfo();
+                bob.WriteMyInfo();
 
+                Console.Write("Enter an amount: ");
+                string? howMuch = Console.ReadLine();
+                if (howMuch == "") return;
+                if (int.TryParse(howMuch, out int cash))
+                {
+                    Console.Write("Who should give the cash: ");
+                    string? whichGuy = Console.ReadLine();
+                    if(whichGuy == "Joe")
+                    {
+                        joe.GiveCash(cash);
+                        bob.ReceiveCash(cash);
+                    }
+                    else if (whichGuy == "Bob")
+                    {
+                        bob.GiveCash(cash);
+                        joe.ReceiveCash(cash);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter 'Joe or 'Bob'.");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Please enter an amount(or blank line to exit).");
+                }
+            }
 
         }
     }
